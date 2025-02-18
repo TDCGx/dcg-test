1083c1849a1582e5fe87d59097e297dfdcc6bc24
Yasir Mohiuddin <yyasirmoh@gmail.com>
2025-02-18 15:06:47 +0800

    new changes to the nav bar

--- components/landing/Navbar.tsx
+++ components/landing/Navbar.tsx
@@ -5,14 +5,55 @@ import Link from "next/link"
 import { cn } from "@/lib/utils"
 import Image from "next/image";
 
+
 //ui components
 import {
   NavigationMenu,
   NavigationMenuItem,
+  NavigationMenuContent,
   NavigationMenuLink,
   NavigationMenuList,
+  NavigationMenuTrigger,
   navigationMenuTriggerStyle,
 } from "@/components/ui/NavigationMenu"; 
+
+const components: { title: string; href: string; description: string }[] = [
+  {
+    title: "Alert Dialog",
+    href: "/docs/primitives/alert-dialog",
+    description:
+      "A modal dialog that interrupts the user with important content and expects a response.",
+  },
+  {
+    title: "Hover Card",
+    href: "/docs/primitives/hover-card",
+    description:
+      "For sighted users to preview content available behind a link.",
+  },
+  {
+    title: "Progress",
+    href: "/docs/primitives/progress",
+    description:
+      "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
+  },
+  {
+    title: "Scroll-area",
+    href: "/docs/primitives/scroll-area",
+    description: "Visually or semantically separates content.",
+  },
+  {
+    title: "Tabs",
+    href: "/docs/primitives/tabs",
+    description:
+      "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
+  },
+  {
+    title: "Tooltip",
+    href: "/docs/primitives/tooltip",
+    description:
+      "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
+  },
+]
 
 //style component
 import "./landing.styles.css";
@@ -30,10 +71,10 @@ const Navbar = () => {
               <Link href="https://www.dcg.earth/" legacyBehavior passHref>
                 <NavigationMenuLink className={navigationMenuTriggerStyle()}>
                   <Image
-                    src="/shared/dcgx-logo.png"
+                    src="/shared/logo.svg"
                     width={26}  
                     height={29}
-                    alt="DCGx Platform Logo"
+                    alt="DCGx Logo"
                     className=""
                   />
                 </NavigationMenuLink>
@@ -42,6 +83,7 @@ const Navbar = () => {
           </div>    
 
           <div className="login-navbar-items">
+
             <NavigationMenuItem>
               <Link href="https://www.dcg.earth/" legacyBehavior passHref>
                 <NavigationMenuLink className={navigationMenuTriggerStyle()}>
@@ -49,6 +91,7 @@ const Navbar = () => {
                 </NavigationMenuLink>
               </Link>
             </NavigationMenuItem>
+
             <NavigationMenuItem>
               <Link href="https://www.dcg.earth/contact" legacyBehavior passHref>
                 <NavigationMenuLink className={navigationMenuTriggerStyle()}>
@@ -56,6 +99,24 @@ const Navbar = () => {
                 </NavigationMenuLink>
               </Link>
             </NavigationMenuItem>
+
+            <NavigationMenuItem>
+              <NavigationMenuTrigger>Components</NavigationMenuTrigger>
+              <NavigationMenuContent>
+                <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+                  {components.map((component) => (
+                    <ListItem
+                      key={component.title}
+                      title={component.title}
+                      href={component.href}
+                    >
+                      {component.description}
+                    </ListItem>
+                  ))}
+                </ul>
+              </NavigationMenuContent>
+          </NavigationMenuItem>
+          
           </div>
 
         </NavigationMenuList>
